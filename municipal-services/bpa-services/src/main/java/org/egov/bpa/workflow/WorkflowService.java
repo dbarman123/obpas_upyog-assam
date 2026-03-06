12ba3a624e06d1510b587d5d5eed80c0888e1a96
sudidey <142474363+sudidey@users.noreply.github.com>
2026-03-06 14:34:31 +0530

    DEVELOPMENT_AUTHORITY added

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/workflow/WorkflowService.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/workflow/WorkflowService.java
@@ -48,9 +48,9 @@ public class WorkflowService {
 		BUSINESS_SERVICE_MAP.put(new AuthorityKey(PlanningPermitAuthorityEnum.DEVELOPMENT_AUTHORITY, BuildingPermitAuthorityEnum.GRAM_PANCHAYAT), "BPA_DA_GP");
 		BUSINESS_SERVICE_MAP.put(new AuthorityKey(PlanningPermitAuthorityEnum.TACP, BuildingPermitAuthorityEnum.GRAM_PANCHAYAT), "BPA_TACP_GP");
 		BUSINESS_SERVICE_MAP.put(new AuthorityKey(PlanningPermitAuthorityEnum.TACP, BuildingPermitAuthorityEnum.MUNICIPAL_BOARD), "BPA_TACP_MB");
-		BUSINESS_SERVICE_MAP.put(new AuthorityKey(PlanningPermitAuthorityEnum.GMDA, BuildingPermitAuthorityEnum.MC), "BPA_GMDA_GMC");
-		BUSINESS_SERVICE_MAP.put(new AuthorityKey(PlanningPermitAuthorityEnum.GMDA, BuildingPermitAuthorityEnum.NGMB), "BPA_GMDA_NGMB");
-		BUSINESS_SERVICE_MAP.put(new AuthorityKey(PlanningPermitAuthorityEnum.GMDA, BuildingPermitAuthorityEnum.GRAM_PANCHAYAT), "BPA_GMDA_GP");
+		BUSINESS_SERVICE_MAP.put(new AuthorityKey(PlanningPermitAuthorityEnum.DA, BuildingPermitAuthorityEnum.MC), "BPA_GMDA_GMC");
+		BUSINESS_SERVICE_MAP.put(new AuthorityKey(PlanningPermitAuthorityEnum.DA, BuildingPermitAuthorityEnum.NGMB), "BPA_GMDA_NGMB");
+		BUSINESS_SERVICE_MAP.put(new AuthorityKey(PlanningPermitAuthorityEnum.DA, BuildingPermitAuthorityEnum.GRAM_PANCHAYAT), "BPA_GMDA_GP");
 	}
 
 	@Autowired
