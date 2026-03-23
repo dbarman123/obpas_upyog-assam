e1a8233570ff1ec1b4608f894d9262ddf547df57
dbarman123 <124787552+dbarman123@users.noreply.github.com>
2026-03-23 18:04:26 +0530

    emp_wf_inbox changes assignee 23032026

--- frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/obpsv2/useBPAV2Inbox.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/obpsv2/useBPAV2Inbox.js
@@ -25,7 +25,8 @@ const useBPAV2Inbox = ({ tenantId, filters, config={} }) => {
     let _filters = {
         tenantId,
         processSearchCriteria: {
-          assignee : user.info.type=="EMPLOYEE"? "": user.info.uuid,
+          // assignee : user.info.type=="EMPLOYEE"? "": user.info.uuid,
+          assignee : user.info.uuid,
           moduleName: "bpa-services", 
           businessService: ["BPA_DA_MB","BPA_DA_GP","BPA_GMDA_GMC","BPA_GMDA_NGMB","BPA_GMDA_GP","BPA_TACP_MB","BPA_TACP_GP"],
         },
